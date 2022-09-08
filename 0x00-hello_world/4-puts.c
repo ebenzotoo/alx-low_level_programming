08c871af903a7f44c5bb0199d2152b26782c3e83
ebenzotoo <ebenzotoo>
2022-09-08 14:46:08 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,16 @@
 #!/bin/bash
-#include <stdio.h>
-
-int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
-
+#include <
+stdio.h
+/**
+* main
+Printing
+*
+* Return: Always 0 (Successful)
+*/
+int main(void)
+{
+puts("
+puts("\\"Programming is like building a multilingual
+puzzle");
+return (0);
+}
