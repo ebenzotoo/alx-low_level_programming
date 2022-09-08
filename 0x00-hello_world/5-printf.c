9a432a06cf4323e14462af09d557fa9480e45e63
ebenzotoo <ebenzotoo>
2022-09-08 14:56:19 -0700

    task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,2 @@
 #include <stdio.h>
-
-int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
-
+int main(void) { printf("with proper grammar, but the outcome is a piece of art,\n"); return (0); }
