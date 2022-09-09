e41507b639b12a327ef61390af1a04b4ff8d7d4e
ebenzotoo <ebenzotoo>
2022-09-09 01:16:44 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,21 @@
 #include <unistd.h>
+
 /**
-* main - Prints a text
-*
-* Return: Always 1 (Success)
-*/
+ * main -Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+
 int main(void)
+
 {
 
-write(2, "and that piece of  art is useful\" - Dora Korpar,
-2015-10-19\n", 59);
-return (1);
-}
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
+
+
+	write(2, str1, 59);
+
+	return (1);
+
+}
